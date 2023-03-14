4479306afd4eb1ffe4eb4957f3bd8d7fccc58b3b
rangaomkaram <omkaram.ranga@gmail.com>
2023-03-14 23:19:09 +0530

    marathon

---
+++ marathon.js
@@ -0,0 +1,27 @@
+console.log("Hello Marathon stream");
+// variable deceleration
+
+let username = "Ranga";
+username = "sesha";
+
+//const 
+//var 
+
+/* 
+1. Lexical Structure in JavaScript.This is structure follows by every programming language
+-> It is elementary rule for language
+-> let's work witn a) Text
+
+*/
+//Texts
+//var ONLINE , var Online, var online -> different variables 
+/* var 1_online  -> never start with numeric
+a)If you want use the numeric then you can prefix the dollar sign before the numeric varaible decleration;
+eg: var $1_online;
+b)Mostly developer uses the camelcasing for the decelaring the variables 
+for const , like never change the value or text in entire application/program the variable deceleration
+will be in all capitals in general. 
+i.e., const PI , const RADII ,  const BOLTMANN TEMP e.t.c
+
+ */
+
