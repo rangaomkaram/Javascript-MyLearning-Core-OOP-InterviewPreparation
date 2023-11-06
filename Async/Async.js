1d95237ac7752aed2294ac88303a37c1e91cd794
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2023-11-06 22:17:39 +0530

    Asynchronous JS

---
+++ Async/Async.js
@@ -0,0 +1,32 @@
+/*
+
+Exceution Context 
+-> line by line
+-> sync
+-> Call Stack
+-> Memory Heap
+-> Global
+
+*/
+
+/*
+  Blocking Code vs Non-Blocking Code
+ */
+
+// Blocking Code
+// Block the flow of the program
+/*
+Read file sync
+like OS - Queue system
+*/
+
+/*
+Non-Blocking Code
+
+-> Does not block execution
+-> Read File Async
+
+Depends on USE case -> we use blocking and Non-Blocking
+Ex : user Register (Block)
+
+*/
