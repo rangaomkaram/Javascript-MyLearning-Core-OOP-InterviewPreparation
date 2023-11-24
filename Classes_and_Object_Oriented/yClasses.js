06267975b30e4cb9a999b67fb8c0b129fde5d647
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2023-11-24 22:34:40 +0530

    concept of classes

---
+++ Classes_and_Object_Oriented/yClasses.js
@@ -0,0 +1,40 @@
+// After ES6 we got class , which is syntatical sugar 
+
+class User  {
+    //consturctor
+    // Properties  and methods
+
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // function decelaration in class
+
+    encryptPassword(){
+        return `${this.password}def`
+    }
+
+    changeUserNameFirstCapLetter(){
+        return `${this.username.charAt(0).toUpperCase() + this.username.slice(1)}`
+    }
+
+    //get captial letter of each word in full name
+
+    captialEachLetterofWord(){
+        this.username = this.username.split(" ");
+        for (let index = 0; index < this.username.length; index++) {
+            this.username[index] = this.username[index].charAt(0).toUpperCase() + this.username[index].slice(1);   
+        }
+        return this.username.join(" ")
+    }
+}
+
+const firstObj = new User("uday","udaykumar@mail.com",345)
+
+// console.log(firstObj.encryptPassword())
+// console.log(firstObj.changeUserNameFirstCapLetter())
+
+const secUser = new User("full name first name last name","uday@mail.com",34534);
+console.log(secUser.captialEachLetterofWord())
