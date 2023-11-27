0c889ba379e594fc1951f6b019a8b446c1b633ac
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2023-11-27 18:57:48 +0530

    getOwnPropertyDescriptor

--- Objects/objPropDesc.js
+++ Objects/objPropDesc.js
@@ -8,5 +8,51 @@
 const Customer = {
     fname : "Raja",
     order : 12345,
-    ProductName: "MiniToyCar"
+    ProductName: "MiniToyCar",
+    orderfunc : function () {
+        console.log(`customer details`);
+        
+    }
 }
+
+const descriptor = Object.getOwnPropertyDescriptor(Customer,"order")
+console.log(descriptor);
+//output: { value: 12345, writable: true, enumerable: true, configurable: true }
+
+
+//We can define the object properties
+// for multiple properties -> defineProperties
+// for single property -> defineProperty
+
+Object.defineProperty(Customer, "order", {
+    writable:false,
+    enumerable: false
+})
+
+
+console.log(Object.getOwnPropertyDescriptor(Customer,"order"))
+
+
+
+// for(let [key, value] of Customer){
+//     console.log(`${key} ,${value}`)
+// }
+// output : Customer is not iterable
+
+// To make the Customer iterable
+
+for(let[key,value] of Object.entries(Customer)){
+    console.log(`${key} :  ${value}`)
+}
+
+//ouput only fname and productname key-pairs will be iterable, since order property we made enumerable: false
+
+//What if the function is there in the object:
+
+//Check the if function is there ->
+
+for (let [key, pair] of Object.entries(Customer)){
+    if(typeof value !== 'function')
+    console.log(`${key} : ${pair}`)
+}
+
