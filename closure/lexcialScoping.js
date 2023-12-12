5190fd212bfc9b38933ffeb3f00e83891584a02e
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2023-12-12 10:09:34 +0530

    concepts of closure and lexcial scoping

--- closure/lexcialScoping.js
+++ closure/lexcialScoping.js
@@ -1,11 +1,31 @@
-function init() {
-    let getName = "GoodName";
+// function init() {
+//     let getName = "GoodName";
 
-    function displayName() {
-        console.log(getName);
+//     function displayName() {
+//         console.log(getName);
+        
+//     }
+//     displayName();
+// }
+
+// init();
+
+function outer() {
+    let getName = "Hey This is outer access";
+
+    function inner() {
+        let innerAccess = "Hey this inner access secret";
+        console.log(`Get the outer :${getName}`);
+    }
+    function innerTwo() {
+        console.log(`innerTwo:,${getName}`);
+        // console.log(innerAccess)
         
     }
-    displayName();
+    inner()
+    innerTwo()
+    
 }
 
-init();
+outer()
+// console.log(`TOO Outer:,${getName}`)
