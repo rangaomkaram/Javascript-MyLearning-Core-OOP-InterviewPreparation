587dc54e41b4d1047413a2b54a8353307b2bd310
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2023-12-14 12:03:15 +0530

    destructing concepts

---
+++ Destructing_Concepts/destructing.js
@@ -0,0 +1,17 @@
+//How to destructure 
+
+// Array :
+
+const user = ["ranga","sesha"]
+
+// using position / indexes
+console.log(user[0]); 
+console.log(user[1]);
+
+
+// Destructure
+
+const [firstname,lastname] = ["ranga","sesha"];
+
+console.log(firstname)
+console.log(lastname)
